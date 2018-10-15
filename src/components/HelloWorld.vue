3879ef92b47509436d6dfd1ab0035be158b6b122
akunnnnn <51312988@qq.com>
2018-10-15 10:54:48 +0800

    slot

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,73 +1,43 @@
-<!--map的运用3-->
+<!--slot的运用-->
 <template>
     <div>
-{{goodsList}}
+      <v-two>
+8888888888888888
+<div slot="rrr"></div>
+      </v-two>
     </div>
 </template>
 <script>
-/* eslint-disable */
-
+/* eslint-disable */ 
+import Vue from "vue";
+Vue.component("v-two", {
+  template: ` <div>
+      <nav>
+        <slot name="nav">nav</slot>
+      </nav>
+      <main>
+        <slot name="main">main</slot>
+      </main>
+      <footer>
+        <slot name="footer">footer</slot>
+      </footer>
+      <slot></slot>
+      <slot name="rrr"></slot>
+    </div>`,
+  props: [],
+  methods: {
+    closeDiv() {
+    }
+  }
+});
 export default {
   data() {
     return {
-      goodsList: [
-        {
-          name: "可乐",
-          brand: "可口可乐公司",
-          location: "海外",
-          size: "300ml",
-          number: 10,
-          send: "海外仓库",
-          price: 3,
-          num: 2
-        },
-        {
-          name: "雪碧",
-          brand: "可口可乐公司",
-          location: "海外",
-          size: "270ml",
-          number: 10,
-          send: "海外仓库",
-          price: 3,
-          num: 1
-        },
-        {
-          name: "芬达",
-          brand: "可口可乐公司",
-          location: "海外",
-          size: "300ml",
-          number: 20,
-          send: "海外仓库",
-          price: 4,
-          num: 4
-        },
-        {
-          name: "美年达",
-          brand: "可口可乐公司",
-          location: "海外",
-          size: "300ml",
-          number: 16,
-          send: "海外仓库",
-          price: 2,
-          num: 3
-        }
-      ]
     };
   },
-  computed: {},
-
-  mounted() {
-    this.goodsList2();
-  },
   methods: {
-    goodsList2: function() {
-      var _this = this;
-      this.goodsList.map(function(goods) {
-        _this.$set(goods, "select", false);
-      });
-    }
+
   }
 };
 </script>
-
 
---
+++ src/components/HelloWorld2.vue
@@ -0,0 +1,73 @@
+<!--map的运用3-->
+<template>
+    <div>
+{{goodsList}}
+    </div>
+</template>
+<script>
+/* eslint-disable */
+
+export default {
+  data() {
+    return {
+      goodsList: [
+        {
+          name: "可乐",
+          brand: "可口可乐公司",
+          location: "海外",
+          size: "300ml",
+          number: 10,
+          send: "海外仓库",
+          price: 3,
+          num: 2
+        },
+        {
+          name: "雪碧",
+          brand: "可口可乐公司",
+          location: "海外",
+          size: "270ml",
+          number: 10,
+          send: "海外仓库",
+          price: 3,
+          num: 1
+        },
+        {
+          name: "芬达",
+          brand: "可口可乐公司",
+          location: "海外",
+          size: "300ml",
+          number: 20,
+          send: "海外仓库",
+          price: 4,
+          num: 4
+        },
+        {
+          name: "美年达",
+          brand: "可口可乐公司",
+          location: "海外",
+          size: "300ml",
+          number: 16,
+          send: "海外仓库",
+          price: 2,
+          num: 3
+        }
+      ]
+    };
+  },
+  computed: {},
+
+  mounted() {
+    this.goodsList2();
+  },
+  methods: {
+    goodsList2: function() {
+      var _this = this;
+      this.goodsList.map(function(goods) {
+        _this.$set(goods, "select", false);
+      });
+    }
+  }
+};
+</script>
+
+

d98d7eee2bb1e79f6e5c74def5fe8a0cd00458d6
akunnnnn <51312988@qq.com>
2018-11-02 14:06:28 +0800

    JSX2
    Signed-off-by: akunnnnn <51312988@qq.com>

---
+++ static/TS7/ee.jsx
@@ -0,0 +1,7 @@
+var MyComponent = /** @class */ (function () {
+    function MyComponent() {
+    }
+    return MyComponent;
+}());
+// `MyComponent`的元素属性类型为`{foo?: string}`
+<MyComponent foo="bar"/>;
